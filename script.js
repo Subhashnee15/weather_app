2effa26ccfe7ba044dff55d52510c9d7e7ad1104
SUBHASHNEE THAKUR <126400709+Subhashnee15@users.noreply.github.com>
2025-03-04 05:52:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,8 @@
     const wind = document.getElementById('wind');
 
     //our openWeatherMap API key 
-    const apikey = 'YOUR_API_KEY'; // Replace with your OpenWeatherMap API key
+    const apiKey = '28eaf950fe22f1cc56f7de4e38f22db4'; 
+// Replace with your OpenWeatherMap API key
 
     //function to fetch weather data
     const getWeather = async(city) => {
@@ -19,7 +20,7 @@
             if(data.cod === 200) {
                 // Updtae the UI with the weather data
                 cityName.textContent = data.name;
-                temperature.textContent = `Temperature: ${data.amin.temp}°C`;
+                temperature.textContent = `Temperature: ${data.main.temp}°C`;
                 description.textContent = `Description:${data.weather[0].description}`;
                 humidity.textContent = `Humidity : ${data.main.humidity}%`;
                 wind.textContent = ` Wind Speed : ${data.wind.speed}m/s`;
@@ -28,8 +29,36 @@
             }
 
         } catch (error) {
-            console.error("Errpr fetching weather data: " , error);
+            console.error("Error fetching weather data: " , error);
         }
+    };
+
+    //function to update background image based on weather condition
+    const updateBackground = (weatherCondition) => {
+        let backgroundImage = '';
+
+        // set background image based on weather condition
+        switch(weatherCondition.toLowerCase()) {
+            case 'clear':
+                backgroundImage = 'url("./images/sunny img.jpeg")';
+                break;
+
+                case 'clouds' :
+                    backgroundImage  = 'url("./images/cloudy.jpeg")';
+                    break;
+                    case 'rain' :
+                        backgroundImage = 'url("./images/rainy.jpeg")';
+                        break;
+                        case 'snow': 
+                        backgroundImage = 'url("./images/snowy.jpeg")';
+                        break;
+                        default :
+                        backgroundImage = 'url("./images/default.jpg")';
+                        break;
+
+        }
+        //apply the background iamge to the body
+        document.body.style.backgroundImage = backgroundImage;
     };
     // Add event listener to search button
     searchButton.addEventListener('click', () => {
